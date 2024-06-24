e5f20ebb8bf2e27ada13b285a5d8091059671fdc
Murodjon Halilov <murodjonhalilov76@gmail.com>
2024-06-25 01:42:12 +0500

    upd

---
+++ bot.js
@@ -0,0 +1,24 @@
+const { Telegraf } = require("telegraf");
+
+const TOKEN = "7162733485:AAE7qf0rbq258QuYM6WepPK4hOLh4htUHZA";
+const bot = new Telegraf(TOKEN);
+
+const web_link = "https://murodcombat.netlify.app/";
+
+bot.start((ctx) => {
+  console.log(ctx.update.message.text);
+  console.log(ctx.update.message.from);
+
+  return ctx.reply(
+    `Assalomu aleykum ${ctx.update.message.from.first_name}! Siz bizning yangi CombatGame 🎮 o'yinimizga tashrif buyurdingiz... \nMuallif: @MurodjonHalilov`,
+    {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "O'yinni boshlash 🎮", web_app: { url: web_link } }],
+        ],
+      },
+    }
+  );
+});
+
+bot.launch();
